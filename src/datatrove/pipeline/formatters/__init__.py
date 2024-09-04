f2e19747d0ef1a8c4629c8204e2a91903be4a266
manuelbrack <manuel.brack@stud.tu-darmstadt.de>
2024-09-03 17:04:20 -0700

    Add replacment formatter

--- src/datatrove/pipeline/formatters/__init__.py
+++ src/datatrove/pipeline/formatters/__init__.py
@@ -1,4 +1,4 @@
 from .ftfy import FTFYFormatter
 from .pii import PIIFormatter
 from .symbol_lines_remover import SymbolLinesFormatter
-from custom import CustomFormatter
+from .custom import CustomFormatter
